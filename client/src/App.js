746f98e8ea5bf670a7811b0557bb07d572ae4747
olegweremyew <olegweremey1994@mail.ru>
2022-06-10 11:09:53 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import './styles/app.scss';
+import Toolbar from "./components/Toolbar";
+import SettingBar from "./components/SettingBar";
+import Canvas from "./components/Canvas";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path='/:id'>
+            <Toolbar/>
+            <SettingBar/>
+            <Canvas/>
+          </Route>
+          <Redirect to={`f${(+new Date).toString(16)}`}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  
+  );
+}
+
+export default App;
